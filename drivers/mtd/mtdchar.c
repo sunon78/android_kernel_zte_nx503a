23f56f201957bc062436eb92c60c978577bd589b
Maegorn <mattia.pellegrino@live.it>
2016-05-22 15:33:53 +0200

    Update to 3.4.50

--- drivers/mtd/mtdchar.c
+++ drivers/mtd/mtdchar.c
@@ -357,9 +357,7 @@ static ssize_t mtdchar_write(struct file *file, const char __user *buf, size_t c
 } /* mtdchar_write */
 
 /*======================================================================
-
     IOCTL calls for getting device parameters.
-
 ======================================================================*/
 static void mtdchar_erase_callback (struct erase_info *instr)
 {
@@ -729,7 +727,6 @@ static int mtdchar_ioctl(struct file *file, u_int cmd, u_long arg)
 			/*
 			  FIXME: Allow INTERRUPTIBLE. Which means
 			  not having the wait_queue head on the stack.
-
 			  If the wq_head is on the stack, and we
 			  leave because we got interrupted, then the
 			  wq_head is no longer there when the
@@ -1163,41 +1160,17 @@ static int mtdchar_mmap(struct file *file, struct vm_area_struct *vma)
 	struct mtd_file_info *mfi = file->private_data;
 	struct mtd_info *mtd = mfi->mtd;
 	struct map_info *map = mtd->priv;
-	resource_size_t start, off;
-	unsigned long len, vma_len;
 
-	if (mtd->type == MTD_RAM || mtd->type == MTD_ROM) {
-		off = get_vm_offset(vma);
-		start = map->phys;
-		len = PAGE_ALIGN((start & ~PAGE_MASK) + map->size);
-		start &= PAGE_MASK;
-		vma_len = get_vm_size(vma);
-
-		/* Overflow in off+len? */
-		if (vma_len + off < off)
-			return -EINVAL;
-		/* Does it fit in the mapping? */
-		if (vma_len + off > len)
-			return -EINVAL;
-
-		off += start;
-		/* Did that overflow? */
-		if (off < start)
-			return -EINVAL;
-		if (set_vm_offset(vma, off) < 0)
-			return -EINVAL;
-		vma->vm_flags |= VM_IO | VM_RESERVED;
-
+	/* This is broken because it assumes the MTD device is map-based
+	  and that mtd->priv is a valid struct map_info.  It should be
+	  replaced with something that uses the mtd_get_unmapped_area()
+	  operation properly. */
+        if (0 /*mtd->type == MTD_RAM || mtd->type == MTD_ROM*/) {
 #ifdef pgprot_noncached
-		if (file->f_flags & O_DSYNC || off >= __pa(high_memory))
+		if (file->f_flags & O_DSYNC || map->phys >= __pa(high_memory))
 			vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 #endif
-		if (io_remap_pfn_range(vma, vma->vm_start, off >> PAGE_SHIFT,
-				       vma->vm_end - vma->vm_start,
-				       vma->vm_page_prot))
-			return -EAGAIN;
-
-		return 0;
+		return vm_iomap_memory(vma, map->phys, map->size);
 	}
 	return -ENOSYS;
 #else
