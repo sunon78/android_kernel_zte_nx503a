848c22eab2148cd3b6abbee467549d5e4da00d28
Maegorn <mattia.pellegrino@live.it>
2016-06-24 18:42:24 +0200

    Add PegasusQ governon and set the enviroment ready to add others

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -15,9 +15,12 @@
  *
  */
 
+#include <asm/cputime.h>
 #include <linux/kernel.h>
+#include <linux/kernel_stat.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/tick.h>
 #include <linux/notifier.h>
 #include <linux/cpufreq.h>
 #include <linux/delay.h>
@@ -141,6 +144,41 @@ void disable_cpufreq(void)
 }
 static LIST_HEAD(cpufreq_governor_list);
 static DEFINE_MUTEX(cpufreq_governor_mutex);
+
+static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+ {
+ 	u64 idle_time;
+ 	u64 cur_wall_time;
+ 	u64 busy_time;
+ 
+ 	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+ 
+ 	busy_time = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
+ 	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
+ 	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
+ 	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
+ 	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
+ 	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+ 
+ 	idle_time = cur_wall_time - busy_time;
+ 	if (wall)
+ 		*wall = cputime_to_usecs(cur_wall_time);
+ 
+ 	return cputime_to_usecs(idle_time);
+ }
+ 
+u64 get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
+{
+	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
+
+	if (idle_time == -1ULL)
+		return get_cpu_idle_time_jiffy(cpu, wall);
+	else
+		idle_time += get_cpu_iowait_time_us(cpu, wall);
+
+	return idle_time;
+}
+ EXPORT_SYMBOL_GPL(get_cpu_idle_time);
 
 static struct cpufreq_policy *__cpufreq_cpu_get(unsigned int cpu, int sysfs)
 {
