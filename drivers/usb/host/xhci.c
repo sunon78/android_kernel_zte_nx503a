cf86cee68ebe8c5a86ff4929fc53c3af3860098f
Maegorn <mattia.pellegrino@live.it>
2016-11-30 13:05:17 +0100

    Update to 3.4.89

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -332,7 +332,7 @@ static void xhci_cleanup_msix(struct xhci_hcd *xhci)
 	return;
 }
 
-static int xhci_try_enable_msi(struct usb_hcd *hcd)
+static inline int xhci_try_enable_msi(struct usb_hcd *hcd)
 {
 	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
 	struct pci_dev  *pdev;
@@ -389,7 +389,7 @@ static int xhci_try_enable_msi(struct usb_hcd *hcd)
 	return 0;
 }
 
-static void xhci_cleanup_msix(struct xhci_hcd *xhci)
+static inline void xhci_cleanup_msix(struct xhci_hcd *xhci)
 {
 }
 
@@ -790,7 +790,7 @@ void xhci_shutdown(struct usb_hcd *hcd)
 #ifdef CONFIG_PM
 
 #ifdef CONFIG_PCI
-static void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
+static inline void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
 {
 	int i;
 
@@ -800,7 +800,7 @@ static void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
 	}
 }
 #else
-static void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
+static inline void xhci_msix_sync_irqs(struct xhci_hcd *xhci)
 {
 }
 #endif /* CONFIG_PCI */
