51d36d100d14f22b287c290312ca55bcae71b952
Maegorn <mattia.pellegrino@live.it>
2017-04-07 12:48:31 +0200

    msm: Add Alucard Hotplug driver

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -345,6 +345,17 @@ static inline unsigned int cpufreq_get(unsigned int cpu)
 }
 #endif
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define MIN_FREQ_LIMIT	300000
+#else
+#define MIN_FREQ_LIMIT	300000
+#endif
+#ifdef CONFIG_CPU_OVERCLOCK
+#define MAX_FREQ_LIMIT	2150400
+#else
+#define MAX_FREQ_LIMIT	2150400
+#endif
+
 /* query the last known CPU freq (in kHz). If zero, cpufreq couldn't detect it */
 #ifdef CONFIG_CPU_FREQ
 unsigned int cpufreq_quick_get(unsigned int cpu);
@@ -360,6 +371,9 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 }
 #endif
 
+enum {
+	BOOT_CPU = 0,
+};
 
 /*********************************************************************
  *                       CPUFREQ DEFAULT GOVERNOR                    *
