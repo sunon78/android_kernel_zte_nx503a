d8d921eede15a9da67409231ce8d819b59491043
Tom Marshall <tdm.code@gmail.com>
2017-05-30 11:44:56 +0200

    kernel: Fix potential refcount leak in su check

--- fs/namei.c
+++ fs/namei.c
@@ -1798,8 +1798,10 @@ static int path_lookupat(int dfd, const char *name,
 	}
 
 	if (!err) {
-		if (d_is_su(nd->path.dentry) && !su_visible())
-			err = -ENOENT;
+		if (d_is_su(nd->path.dentry) && !su_visible()) {
+				path_put(&nd->path);
+				err = -ENOENT;
+			}
 	}
 
 	if (base)
